253db9c316e7839adc430c2291fbb9a7a955cca4
Sahil Makadia <makadiasahil9846@gmail.com>
2023-11-05 18:16:11 +0530

    sending OTP verification  mail using nodemailer

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const User = require("../models/userModel.js")
 const nodemailer = require("nodemailer")
+const EmailVerificationToken = require("../models/emailVerificationToken.js")
 
 const createUser = async (req, res) => {
     const { name, email, password } = req.body
@@ -8,18 +9,39 @@ const createUser = async (req, res) => {
     if (fetchedUser) return res.status(401).json({ error: "Email already exists." })
 
     const newUser = new User({ name, email, password });
-    await newUser.save()
+    await newUser.save();
 
-    // var transport = nodemailer.createTransport({
-    //     host: "sandbox.smtp.mailtrap.io",
-    //     port: 2525,
-    //     auth: {
-    //       user: "1c66f85c993e3c",
-    //       pass: "a4feaee91ce23c"
-    //     }
-    //   });
+    // Generate 6 Digit OTP
+    let OTP = "";
+    for (let i = 0; i <= 5; i++) {
+        const randomVal = Math.round(Math.random() * 10);
+        OTP += randomVal
+    }
+    // save OTP in db with user id
+    const newEmailVerificationToken = new EmailVerificationToken({ owner: newUser._id, token: OTP });
+    await newEmailVerificationToken.save();
 
-    res.status(201).json({ user: newUser });
+    // Send this otp to user's mail
+    var transport = nodemailer.createTransport({
+        host: "sandbox.smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+            user: "1c66f85c993e3c",
+            pass: "a4feaee91ce23c"
+        }
+    });
+
+    transport.sendMail({
+        from: 'verification@MVR.com',
+        to: newUser.email,
+        subject: 'Email verification',
+        html: `
+        <h1>Your Verification OTP</h1>
+        <h4>${OTP}</h4>
+        `
+    })
+
+    res.status(201).json({ message: "Please Verify Your Email. OTP Has Been Sent To Your Email Account." });
 }
 
 
