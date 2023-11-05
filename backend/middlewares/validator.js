2ad5e29d197c82f33e1467e6c1ec0941d60d372b
Sahil Makadia <makadiasahil9846@gmail.com>
2023-11-05 14:19:06 +0530

    Express-validator and Validate middle ware implemented

---
+++ backend/middlewares/validator.js
@@ -0,0 +1,17 @@
+const { check, validationResult } = require("express-validator")
+
+
+
+exports.userValidations = [check("name").trim().not().isEmpty().withMessage("Name is missing."),
+check("email").normalizeEmail().isEmail().withMessage("Email is missing"),
+check("password").trim().not().isEmpty().withMessage("Password is missing.").isLength({ min: 8, max: 20 }).withMessage("Password must be 8 to 20 characters long!")]
+
+
+exports.validate = (req, res, next) => {
+    const errors = validationResult(req).array();
+    if (errors.length) {
+        return res.json({ errors: errors[0].msg })
+    }
+
+    next();
+}
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const createUser = require("../controllers/userController");
+const { userValidations, validate } = require("../middlewares/validator");
 
 const userRouter = express.Router();
 
 
-userRouter.post("/create", createUser);
+userRouter.post("/create", userValidations, validate, createUser);
 
 
 module.exports = userRouter;
