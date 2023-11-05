5a4bffa59ce6aeade28d3ae39de02c5002c33327
Sahil Makadia <makadiasahil9846@gmail.com>
2023-11-05 13:27:27 +0530

    Storing user in database with create route in userRoute, Changes in db/index.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,14 @@
-const createUser = (req, res) => {
-    console.log(req.body)
-    res.send({ user: req.body });
+const User = require("../models/userModel.js")
+
+const createUser = async (req, res) => {
+    const { name, email, password } = req.body
+
+    const newUser = new User({ name, email, password });
+    newUser.save()
+
+    await res.send({ user: newUser });
 }
+
 
 
 module.exports = createUser;
--- backend/db/index.js
+++ backend/db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.6/mvr_DB").then(() => {
+mongoose.connect("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.6", ({ dbName: "MVR_DB" })).then(() => {
     console.log("Database connected.")
 }).catch((err) => {
     console.log("Failed to connect : ", err)
