831b226ba3f410d9d1a3093e63ade1bd3f13abef
Sahil Makadia <makadiasahil9846@gmail.com>
2023-11-07 08:39:05 +0530

    Password Reset Model Created

---
+++ backend/models/passwordResetToken.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const passwordResetTokenSchema = mongoose.Schema({
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    token: {
+        type: String,
+        required: true
+    },
+    createAt: {
+        type: Date,
+        expires: 3600,
+        default: Date.now()
+    }
+});
+
+// Hashing The token
+passwordResetTokenSchema.pre("save", async function (next) {
+    if (this.isModified("token")) {
+        this.token = await bcrypt.hash(this.token, 10);
+    }
+
+    next();
+});
+
+// this is how custom method can be implemented
+// here this compareToken is used for comparing hashed token and token entered by user.
+passwordResetTokenSchema.methods.compareToken = async function (token) {
+    const result = await bcrypt.compare(token, this.token);
+    return result
+}
+
+module.exports = mongoose.model("PasswordResetToken", passwordResetTokenSchema)
