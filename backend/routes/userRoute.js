6c3ce62d4659c55babd7e46c556500ca39bceb69
Sahil Makadia <makadiasahil9846@gmail.com>
2023-11-07 10:01:55 +0530

    UserRoute With some changes

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { createUser, verifyEmail, resendEmailVerificationToken, forgetPassword } = require("../controllers/userController");
 const { userValidations, validate } = require("../middlewares/validator");
+const { isValidPassToken } = require("../middlewares/user");
 
 const userRouter = express.Router();
 
@@ -9,5 +10,8 @@ userRouter.post("/create", userValidations, validate, createUser);
 userRouter.post("/verify-email", verifyEmail);
 userRouter.post("/resend-email-verification-token", resendEmailVerificationToken);
 userRouter.post("/reset-password", forgetPassword);
+userRouter.post("/verify-password-reset-token", isValidPassToken, (req, res) => {
+    res.json({ valid: "true" })
+});
 
 module.exports = userRouter;
