4d9d7c5eb0a3d242a41e7f36eeaeb700738e7c96
Sahil Makadia <makadiasahil9846@gmail.com>
2023-11-12 09:45:08 +0530

    errorHandler middleware created and Also installed express-async-errors packages for handle async errors.

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,20 @@
 const express = require("express");
+require("express-async-errors"); // for handling async errors
 const morgan = require("morgan");
 require("dotenv").config();
 const userRouter = require("./routes/userRoute.js");
+const { errorHandler } = require("./middlewares/errorHandler.js");
 require("./db")
 
 const app = express();
 app.use(express.json())
 app.use(morgan('dev'))
 app.use("/api/user", userRouter)
+
+
+// error handler middleware
+app.use(errorHandler)
+
 
 
 app.listen(5000, () => {
