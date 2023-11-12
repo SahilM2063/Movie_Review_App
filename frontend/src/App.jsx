904340c657499edad890f8b9decd21dd0ca2243d
Sahil Makadia <makadiasahil9846@gmail.com>
2023-11-12 20:33:51 +0530

    Frontend setup with react project, tailwind and daisyUI, Also Favicon and Logo changed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="btn btn-primary">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,58 @@
+import daisyui from "daisyui"
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      {
+        mytheme1: {
+          "primary": "#8af466",
+          "secondary": "#dcc4ff",
+          "accent": "#d243e8",
+          "neutral": "#242638",
+          "base-100": "#3d3b4a",
+          "info": "#9db0f6",
+          "success": "#106556",
+          "warning": "#d09816",
+          "error": "#f25345",
+        }
+      },
+      "light",
+      "dark",
+      "cupcake",
+      "bumblebee",
+      "emerald",
+      "corporate",
+      "synthwave",
+      "retro",
+      "cyberpunk",
+      "valentine",
+      "halloween",
+      "garden",
+      "forest",
+      "aqua",
+      "lofi",
+      "pastel",
+      "fantasy",
+      "wireframe",
+      "black",
+      "luxury",
+      "dracula",
+      "cmyk",
+      "autumn",
+      "business",
+      "acid",
+      "lemonade",
+      "night",
+      "coffee",
+      "winter",
+    ],
+  },
+}
+
