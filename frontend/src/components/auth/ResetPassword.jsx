0c61c6417c5ff7d986e711c7c4853fe7d3509b76
Sahil Makadia <makadiasahil9846@gmail.com>
2023-11-19 14:55:25 +0530

    Verifying password reset token and navigating user based on that

--- frontend/src/components/auth/ResetPassword.jsx
+++ frontend/src/components/auth/ResetPassword.jsx
@@ -1,12 +1,36 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from "react";
-import { Link, useSearchParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useSearchParams, useNavigate } from "react-router-dom";
+import { verifyPasswordResetToken } from "../../api/auth";
+import { useNotification } from "../../hooks";
 
 const ResetPassword = () => {
   const [isVerifying, setIsVerifying] = useState(true);
+  const [isValid, setIsValid] = useState(false);
   const [searchParams] = useSearchParams();
   const token = searchParams.get("token");
   const id = searchParams.get("id");
+
+  const updateNotification = useNotification();
+  const navigate = useNavigate();
+
+  const isValidToken = async () => {
+    const { error, valid } = await verifyPasswordResetToken(token, id);
+    setIsVerifying(false);
+
+    if (error) return updateNotification("error", error);
+
+    if (!valid) {
+      setIsValid(false);
+      return navigate("/auth/reset-password", { replace: true });
+    }
+
+    setIsValid(true);
+  };
+
+  useEffect(() => {
+    isValidToken();
+  }, []);
 
   if (isVerifying) {
     return (
@@ -19,6 +43,14 @@ const ResetPassword = () => {
           <span className="loading loading-ring loading-md"></span>
           <span className="loading loading-ring loading-lg"></span>
         </div>
+      </div>
+    );
+  }
+
+  if (!isValid) {
+    return (
+      <div className="fixed inset-0 flex flex-col gap-3 items-center justify-center -z-10 px-10 md:px-5 sm:px-2 xs:px-1">
+        <h1 className="text-center text-xl">Sorry, Your token is invalid</h1>
       </div>
     );
   }
