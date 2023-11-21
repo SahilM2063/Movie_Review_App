cb1dab704d7023893e18bde06dc0e532cbeb8b25
Sahil Makadia <makadiasahil9846@gmail.com>
2023-11-21 16:30:57 +0530

    Update Actor route and controller added, Also Folder system added on cloudinary

--- backend/controllers/actorController.js
+++ backend/controllers/actorController.js
@@ -1,5 +1,7 @@
 const cloudinary = require("cloudinary").v2;
-const Actor = require("../models/actorModel.js")
+const { isValidObjectId } = require("mongoose");
+const Actor = require("../models/actorModel.js");
+const { sendError } = require("../utils/helper.js")
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
@@ -15,7 +17,9 @@ exports.createActor = async (req, res) => {
     const newActor = new Actor({ name, about, gender });
 
     if (file) {
-        const { secure_url, public_id } = await cloudinary.uploader.upload(file.path)
+        const { secure_url, public_id } = await cloudinary.uploader.upload(file.path, {
+            folder: "MVR_Actors"
+        })
 
         newActor.avatar = { url: secure_url, public_id }
     }
@@ -23,3 +27,51 @@ exports.createActor = async (req, res) => {
     await newActor.save();
     res.status(201).json({ id: newActor._id, name, about, gender, avatar: newActor.avatar?.url });
 };
+
+
+exports.updateActor = async (req, res) => {
+    const { name, about, gender } = req.body;
+    const { file } = req;
+    const { actorId } = req.params;
+
+    if (!isValidObjectId(actorId)) return sendError(res, "Invalid request")
+
+    const fetchedActor = await Actor.findById(actorId);
+
+    if (!fetchedActor) return sendError(res, "Invalid request, actor not found", 404)
+
+    const public_id = fetchedActor.avatar?.public_id
+    console.log(public_id)
+
+    if (public_id && file) {
+        const { result } = await cloudinary.uploader.destroy(public_id, {
+            folder: "MVR_Actors"
+        })
+        // console.log(result)
+        if (result !== 'ok') {
+            return sendError(res, "Could not delete image from cloud!")
+        }
+    }
+
+    if (file) {
+        const { secure_url, public_id } = await cloudinary.uploader.upload(file.path, {
+            folder: "MVR_Actors"
+        });
+        fetchedActor.avatar = { url: secure_url, public_id }
+    }
+
+    fetchedActor.name = name;
+    fetchedActor.about = about;
+    fetchedActor.gender = gender;
+
+
+    await fetchedActor.save();
+
+    res.status(201).json({
+        id: fetchedActor._id,
+        name,
+        about,
+        gender,
+        avatar: fetchedActor.avatar?.url
+    })
+}
--- backend/routes/actorRoute.js
+++ backend/routes/actorRoute.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { createActor } = require("../controllers/actorController");
+const { createActor, updateActor } = require("../controllers/actorController");
 const { uploadImage } = require("../middlewares/multer");
 const { actorInfoValidator, validate } = require("../middlewares/validator");
 
 const actorRouter = express.Router();
 
 actorRouter.post("/create-actor", uploadImage.single('avatar'), actorInfoValidator, validate, createActor)
+actorRouter.post("/update-actor/:actorId", uploadImage.single('avatar'), actorInfoValidator, validate, updateActor);
+
 
 module.exports = actorRouter;
