d36669f56d72d226fd4f15d25ec566cb00accac8
Sahil Makadia <makadiasahil9846@gmail.com>
2023-11-21 15:26:25 +0530

    handling if actor doesn't have an image

--- backend/controllers/actorController.js
+++ backend/controllers/actorController.js
@@ -13,11 +13,13 @@ exports.createActor = async (req, res) => {
     const { file } = req;
 
     const newActor = new Actor({ name, about, gender });
-    console.log(file);
 
-    const { secure_url, public_id } = await cloudinary.uploader.upload(file.path)
+    if (file) {
+        const { secure_url, public_id } = await cloudinary.uploader.upload(file.path)
 
-    newActor.avatar = { url: secure_url, public_id }
+        newActor.avatar = { url: secure_url, public_id }
+    }
+
     await newActor.save();
-    res.status(201).json(newActor);
+    res.status(201).json({ id: newActor._id, name, about, gender, avatar: newActor.avatar?.url });
 };
