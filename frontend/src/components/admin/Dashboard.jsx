7d05c9dbb45790a3519d5dc02c1c82425e7f7012
Sahil Makadia <makadiasahil9846@gmail.com>
2023-12-11 21:31:02 +0530

    TrailerUploadModal Is separated now , Also working of Opening and closing the modal

--- frontend/src/components/admin/Dashboard.jsx
+++ frontend/src/components/admin/Dashboard.jsx
@@ -2,16 +2,11 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from "react";
 // import { FiPlus } from "react-icons/fi";
-import { useNotification } from "../../hooks";
 import { HiOutlineMoon } from "react-icons/hi2";
 import { LuSunMedium } from "react-icons/lu";
-import { IoCloudUploadOutline } from "react-icons/io5";
-import { FileUploader } from "react-drag-drop-files";
-import { uploadTrailer } from "../../api/movie";
+import TrailerUploadModal from "./TrailerUploadModal";
 
 const Dashboard = ({ ToggleTheme }) => {
-  const updateNotification = useNotification();
-
   const [theme, setTheme] = useState(
     localStorage.getItem("theme") ?? "mytheme1"
   );
@@ -26,18 +21,6 @@ const Dashboard = ({ ToggleTheme }) => {
   const toggleTheme = () => {
     setTheme(theme === "mytheme1" ? "lemonade" : "mytheme1");
     setThemeTgBtn(theme === "mytheme1" ? false : true);
-  };
-
-  const handleChange = async (file) => {
-    const formData = new FormData();
-    formData.append("trailer", file);
-    const res = await uploadTrailer(formData);
-    console.log(res);
-  };
-
-  const handleTypeError = (error) => {
-    console.log(error);
-    updateNotification("error", error);
   };
 
   useEffect(() => {
@@ -75,9 +58,9 @@ const Dashboard = ({ ToggleTheme }) => {
           <ul className="p-2 shadow menu right-0 dropdown-content z-[1] mt-2 gap-1 bg-base-100 rounded-sm w-36">
             <li>
               <button
-                onClick={() =>
-                  document.getElementById("Movie_model").showModal()
-                }
+                onClick={() => {
+                  document.getElementById("Movie_model").showModal();
+                }}
               >
                 Add Movie
               </button>
@@ -88,27 +71,7 @@ const Dashboard = ({ ToggleTheme }) => {
           </ul>
         </details>
       </div>
-      <dialog id="Movie_model" className="modal">
-        <div className="modal-box rounded-sm">
-          <form method="dialog">
-            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
-              ✕
-            </button>
-            <FileUploader
-              handleChange={handleChange}
-              onTypeError={handleTypeError}
-              types={["mp4", "avi"]}
-            >
-              <div className="w-40 h-40 border-dashed border rounded-full m-auto flex items-center justify-center cursor-pointer flex-col">
-                <IoCloudUploadOutline size={44} />
-                <p className="text-xs text-center">
-                  Drag and Drop Your File Here!
-                </p>
-              </div>
-            </FileUploader>
-          </form>
-        </div>
-      </dialog>
+      <TrailerUploadModal id={"Movie_model"} className={"modal"} />
     </div>
   );
 };
---
+++ frontend/src/components/admin/TrailerUploadModal.jsx
@@ -0,0 +1,47 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { IoCloudUploadOutline } from "react-icons/io5";
+import { FileUploader } from "react-drag-drop-files";
+import { uploadTrailer } from "../../api/movie";
+import { useNotification } from "../../hooks";
+
+const TrailerUploadModal = () => {
+  const updateNotification = useNotification();
+
+  const handleChange = async (file) => {
+    const formData = new FormData();
+    formData.append("trailer", file);
+    const res = await uploadTrailer(formData);
+    console.log(res);
+  };
+
+  const handleTypeError = (error) => {
+    console.log(error);
+    updateNotification("error", error);
+  };
+  return (
+    <dialog id="Movie_model" className="modal">
+      <div className="modal-box rounded-sm">
+        <form method="dialog">
+          <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+            ✕
+          </button>
+          <FileUploader
+            handleChange={handleChange}
+            onTypeError={handleTypeError}
+            types={["mp4", "avi"]}
+          >
+            <div className="w-40 h-40 border-dashed border rounded-full m-auto flex items-center justify-center cursor-pointer flex-col">
+              <IoCloudUploadOutline size={44} />
+              <p className="text-xs text-center">
+                Drag and Drop Your File Here!
+              </p>
+            </div>
+          </FileUploader>
+        </form>
+      </div>
+    </dialog>
+  );
+};
+
+export default TrailerUploadModal;
