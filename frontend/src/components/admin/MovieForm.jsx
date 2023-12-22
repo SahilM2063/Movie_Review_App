7b06b0e26c10fd1777a5438bb23b3fdc82d977ba
Sahil Makadia <makadiasahil9846@gmail.com>
2023-12-22 11:00:51 +0530

    UI of TagInput Field Implemented For TagInput Field.

--- frontend/src/components/admin/MovieForm.jsx
+++ frontend/src/components/admin/MovieForm.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { Link } from "react-router-dom";
+import TagInput from "./TagInput";
 
 const MovieForm = () => {
   return (
     <div>
-      <h1 className="text-center text-xl font-semibold">Sign Up</h1>
+      <h1 className="text-center text-xl font-semibold">Add movie</h1>
       <form className="card-body p-0 flex gap-4 flex-row">
         <div className="w-[70%]">
           <div className="form-control">
@@ -32,32 +33,10 @@ const MovieForm = () => {
               className="input input-bordered outline-none rounded-sm px-2 text-xs pt-2 resize-none h-24"
             ></textarea>
           </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text text-sm">Password</span>
-            </label>
-            <input
-              type="password"
-              name="password"
-              placeholder="password"
-              className="input input-bordered outline-none rounded-sm px-2 h-9 text-xs"
-            />
-            <label className="label my-1">
-              <p className="label-text-alt">
-                Already have an account ?{" "}
-                <Link
-                  to={"/auth/sign-in"}
-                  className="label-text-alt link link-hover"
-                >
-                  {" "}
-                  Sign in
-                </Link>
-              </p>
-            </label>
-          </div>
-          <div className="form-control">
+          <TagInput />
+          <div className="form-control mt-4">
             <button className="btn btn-primary px-3 min-h-8 h-9 rounded-sm text-xs">
-              Sign Up
+              Create
             </button>
           </div>
         </div>
--- frontend/src/components/admin/TagInput.jsx
+++ frontend/src/components/admin/TagInput.jsx
@@ -2,7 +2,25 @@
 import React from "react";
 
 const TagInput = () => {
-  return <div>TagInput</div>;
+  return (
+    <div>
+      <div className="form-control">
+        <label className="label">
+          <span className="label-text text-[12px] leading-4">Title</span>
+        </label>
+        <div className="flex flex-wrap items-center gap-1 input input-bordered outline-none rounded-sm px-2 text-sm">
+          <span>Tag one</span>
+          <span>Tag one</span>
+          <input
+            type="text"
+            name="title"
+            placeholder="Movie title"
+            className="input p-0 focus:outline-none flex-grow outline-none rounded-sm h-9 text-xs"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TagInput;
