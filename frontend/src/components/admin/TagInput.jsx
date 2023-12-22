5399d005927e6d6f3a6f1aa41e608110ea028fb7
Sahil Makadia <makadiasahil9846@gmail.com>
2023-12-22 11:13:07 +0530

    Separate Tag Component Created For Various Tags

--- frontend/src/components/admin/TagInput.jsx
+++ frontend/src/components/admin/TagInput.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from "react";
+import { IoCloseOutline } from "react-icons/io5";
 
 const TagInput = () => {
   return (
@@ -9,13 +11,12 @@ const TagInput = () => {
           <span className="label-text text-[12px] leading-4">Title</span>
         </label>
         <div className="flex flex-wrap items-center gap-1 input input-bordered outline-none rounded-sm px-2 text-sm">
-          <span>Tag one</span>
-          <span>Tag one</span>
+          <Tag tagName={"hello"} onClickFunc={onclick}/>
           <input
             type="text"
-            name="title"
-            placeholder="Movie title"
-            className="input p-0 focus:outline-none flex-grow outline-none rounded-sm h-9 text-xs"
+            name="tagField"
+            placeholder="Tag 1, Tag 2"
+            className="input p-0 px-1 focus:outline-none flex-grow outline-none rounded-sm h-9 text-xs"
           />
         </div>
       </div>
@@ -24,3 +25,16 @@ const TagInput = () => {
 };
 
 export default TagInput;
+
+const Tag = ({ tagName, onClickFunc }) => {
+  return (
+    <span className="kbd gap-[2px] px-[6px] rounded-md">
+      <p>{tagName}</p>
+      <IoCloseOutline
+        size={16}
+        className="cursor-pointer"
+        onClick={onClickFunc}
+      />
+    </span>
+  );
+};
