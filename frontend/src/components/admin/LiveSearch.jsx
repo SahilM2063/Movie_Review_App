37bd66fe1eb0fc8f1ccc7280b111568bef8a8e02
Sahil Makadia <makadiasahil9846@gmail.com>
2023-12-25 15:21:12 +0530

    Refactored some components and HandleOnFocus and HandleOnBlur Method Added

--- frontend/src/components/admin/LiveSearch.jsx
+++ frontend/src/components/admin/LiveSearch.jsx
@@ -1,5 +1,6 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useState } from "react";
 
 export const profileData = [
   {
@@ -25,38 +26,61 @@ export const profileData = [
 ];
 
 const LiveSearch = () => {
+  const [displaySearch, SetDisplaySearch] = useState(false);
+
+  const handleOnFocus = () => {
+    if (profileData.length > 0) SetDisplaySearch(true);
+  };
+
+  const handleOnBlur = () => {
+    SetDisplaySearch(false);
+  };
+
   return (
     <div className="form-control relative">
       <label className="label">
-        <span className="label-text text-[12px] leading-4">LiveSearch</span>
+        <span className="label-text text-[12px] leading-4">Live Search</span>
       </label>
       <input
         type="text"
         name="profiles"
         placeholder="Search profiles..."
         className="input input-bordered outline-none rounded-sm px-2 h-9 text-xs"
+        onFocus={handleOnFocus}
+        onBlur={handleOnBlur}
       />
-      <div className="w-full max-h-20 mt-1 bg-base-200 top-20 custom-scrollbar overflow-scroll rounded-sm overflow-x-hidden">
-        {profileData.map(({ id, name, avatar }) => {
-          return (
-            <div
-              key={id}
-              className="flex items-center justify-between px-2 py-1 cursor-pointer hover:bg-base-300"
-            >
-              <div className="flex items-center">
-                <img
-                  src={avatar}
-                  alt={name}
-                  className="rounded-full w-8 h-8 mr-3"
-                />
-                <span className="font-semibold">{name}</span>
-              </div>
-            </div>
-          );
-        })}
-      </div>
+      <SearchResultsDropdown
+        visible={displaySearch}
+        profileData={profileData}
+      />
     </div>
   );
 };
 
 export default LiveSearch;
+
+const SearchResultsDropdown = ({ visible, profileData }) => {
+  if (!visible) return null;
+
+  return (
+    <div className="w-full max-h-20 mt-1 bg-base-200 top-20 custom-scrollbar overflow-scroll rounded-sm overflow-x-hidden">
+      {profileData.map(({ id, name, avatar }) => {
+        return (
+          <div
+            key={id}
+            className="flex items-center justify-between px-2 py-1 cursor-pointer hover:bg-base-300"
+          >
+            <div className="flex items-center">
+              <img
+                src={avatar}
+                alt={name}
+                className="rounded-full w-8 h-8 mr-3"
+              />
+              <span className="font-semibold">{name}</span>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
