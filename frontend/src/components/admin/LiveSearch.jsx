90cd8fa88b0cf06df267a3c8c3d6a46b07f4fa63
Sahil Makadia <makadiasahil9846@gmail.com>
2023-12-25 15:07:23 +0530

    Rendering Fake Actors DataProfiles With UI

--- frontend/src/components/admin/LiveSearch.jsx
+++ frontend/src/components/admin/LiveSearch.jsx
@@ -1,6 +1,29 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 
+export const profileData = [
+  {
+    id: 1,
+    name: "Alice Smith",
+    avatar: "https://randomuser.me/api/portraits/women/68.jpg",
+  },
+  {
+    id: 2,
+    name: "Bob Johnson",
+    avatar: "https://randomuser.me/api/portraits/men/12.jpg",
+  },
+  {
+    id: 3,
+    name: "Charlie Brown",
+    avatar: "https://randomuser.me/api/portraits/men/61.jpg",
+  },
+  {
+    id: 4,
+    name: "Diana Miller",
+    avatar: "https://randomuser.me/api/portraits/women/9.jpg",
+  },
+];
+
 const LiveSearch = () => {
   return (
     <div className="form-control relative">
@@ -13,12 +36,24 @@ const LiveSearch = () => {
         placeholder="Search profiles..."
         className="input input-bordered outline-none rounded-sm px-2 h-9 text-xs"
       />
-      <div className="w-full px-2 max-h-20 mt-1 bg-base-200 top-20 custom-scrollbar overflow-scroll rounded-sm overflow-x-hidden">
-        <p>one</p>
-        <p>two</p>
-        <p>two</p>
-        <p>two</p>
-        <p>three</p>
+      <div className="w-full max-h-20 mt-1 bg-base-200 top-20 custom-scrollbar overflow-scroll rounded-sm overflow-x-hidden">
+        {profileData.map(({ id, name, avatar }) => {
+          return (
+            <div
+              key={id}
+              className="flex items-center justify-between px-2 py-1 cursor-pointer hover:bg-base-300"
+            >
+              <div className="flex items-center">
+                <img
+                  src={avatar}
+                  alt={name}
+                  className="rounded-full w-8 h-8 mr-3"
+                />
+                <span className="font-semibold">{name}</span>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
