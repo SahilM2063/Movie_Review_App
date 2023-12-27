788ca91cac91e329751678e141a22d0ed6df2152
Sahil Makadia <makadiasahil9846@gmail.com>
2023-12-27 20:29:03 +0530

    Managing Movie Form State And Handling values of form

--- frontend/src/components/admin/MovieForm.jsx
+++ frontend/src/components/admin/MovieForm.jsx
@@ -1,13 +1,36 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useState } from "react";
 import TagInput from "./TagInput";
 import LiveSearch from "./LiveSearch";
 
+const defaultMovieInfo = {
+  title: "",
+  storyLine: "",
+  tags: [],
+  cast: [],
+  director: {},
+  writers: [],
+  releaseDate: "",
+  poster: null,
+  genres: [],
+  type: "",
+  language: "",
+  status: "",
+};
+
 const MovieForm = () => {
+  const [movieInfo, setMovieInfo] = useState({ ...defaultMovieInfo });
+
   const handleSubmit = (e) => {
     e.preventDefault();
   };
 
+  const handleChange = ({ target }) => {
+    const { value, name } = target;
+    setMovieInfo({...movieInfo, [name]: value });
+  };
+
+  const { title, storyLine, director } = movieInfo;
   return (
     <div>
       <h1 className="text-center text-xl font-semibold">Add movie</h1>
@@ -23,6 +46,8 @@ const MovieForm = () => {
             <input
               type="text"
               name="title"
+              value={title}
+              onChange={handleChange}
               placeholder="Movie title"
               className="input input-bordered outline-none rounded-sm px-2 h-9 text-xs"
             />
@@ -34,13 +59,15 @@ const MovieForm = () => {
               </span>
             </label>
             <textarea
-              name="storyline"
+              name="storyLine"
+              value={storyLine}
+              onChange={handleChange}
               placeholder="Movie description"
               rows={10}
               className="input input-bordered outline-none rounded-sm px-2 text-xs pt-2 resize-none h-24"
             ></textarea>
           </div>
-          <TagInput />
+          <TagInput name="tags" />
           <LiveSearch />
           <div className="form-control mt-4">
             <button className="btn btn-primary px-3 min-h-8 h-9 rounded-sm text-xs">
